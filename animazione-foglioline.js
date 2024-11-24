d5f128b61012c5944230088e5cc8f05c689db390
Jess <160743349+Ctrlaltcant@users.noreply.github.com>
2024-11-24 15:47:17 +0100

    Update animazione-foglioline.js

--- animazione-foglioline.js
+++ animazione-foglioline.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
     const numberOfLeaves = 18; // Numero di foglioline
-    const leafImage = "foglia.png"; // Percorso immagine della foglia
+    const leafImage = "./foglia.png"; // Percorso immagine della foglia
     const screenWidth = window.innerWidth;
     const screenHeight = window.innerHeight;
 
